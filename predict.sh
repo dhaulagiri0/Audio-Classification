1e9ce9ee3d976d3d493fb00c36e1d0ca9e3e4219
meeps123 <22002891+meeps123@users.noreply.github.com>
2021-06-17 01:00:10 +0800

    Updated gitignore

--- predict.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source $HOME/venvs/til2021/bin/activate
-
-python predict_efficient.py --model_fn=runs/colab_efficientnetv2l/best_val_acc.h5 --src_dir=../s1_test_cleaned --dt=1.0 --sr=22050
